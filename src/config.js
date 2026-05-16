437bc591124bdf4c976de19067b8c3db89805f41
SaiBharadhwaj-1415 <vsaibharadhwaj@gmail.com>
2026-05-16 23:55:32 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 // src/config.js
-
 // 👇 Replace this only if your backend runs on a different port or domain.
 // Default FastAPI dev port is 8000
 export const REACT_BACKEND_URL = "https://stranger077-financ.hf.space";
